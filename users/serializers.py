94bf69c8ad30ae40c106e3dfe4157238b6c4d476
Lee DoYoon <ehdbs1276@naver.com>
2023-05-10 16:45:23 +0900

    Add user email check
    유저가 가입할 떄 사용한 email로 인증 메일이 보내지고, 메일에 있는 링크를
    누르면 인증이 되면서 해당 계정을 사용할 수 있는 기능입니다.
    
    views.py
    - add UserActivate
    
    serializers.py
    - edit UserSerializer create
    
    settings.py
    
    urls.py
    - add namespace="user"
    
    users/urls.py
    - add UserActivate
    - add app_name = 'users'
    
    개인 로컬에서 secret.json에email주소와 password추가 필요

--- users/serializers.py
+++ users/serializers.py
@@ -4,6 +4,13 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from posts.serializers import PostSerializer
 
 
+from django.utils.http import urlsafe_base64_encode
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_bytes
+from django.urls import reverse
+from rest_framework_jwt.settings import api_settings
+
+
 class UserSerializer(serializers.ModelSerializer):
     user_posts = PostSerializer(many=True, read_only=True)
     
@@ -11,12 +18,30 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = "__all__"
 
+
     def create(self, validated_data):
         user = super().create(validated_data)
         password = user.password
         user.set_password(password)
+        # is_active = False로 지정해주어 email 인증을 하기 전에 접속 불가능하게 설정
+        user.is_active = False
         user.save()
-        return user
+
+        # jwt 토큰 생성
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+
+        # email을 생성하여 보내는 부분
+        message = f"{user.email}님 링크를 클릭해 계정을 활성화 해주세요\n"
+        # message += f"http://127.0.0.1:8000{reverse('user:activate', kwargs={'uidb64': urlsafe_base64_encode(force_bytes(user.pk)), 'token': account_activation_token.make_token(user)})}"
+        message += f"http://127.0.0.1:8000{reverse('user:activate', kwargs={'uidb64': urlsafe_base64_encode(force_bytes(user.pk)), 'token': token})}"
+        email = EmailMessage('test', message, to=[user.email])
+        email.send()
+
+        return validated_data
+    
         
     def update(self, validated_data):
         user = super().create(validated_data)
--- users/views.py
+++ users/views.py
@@ -6,6 +6,15 @@ from users.serializers import UserSerializer, UserDetailSerializer
 from users.models import User
 from rest_framework.generics import get_object_or_404
 
+# ===================================== email 요청 import 추가 =============================
+import traceback
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_str
+
+import jwt
+from django.conf import settings
+secret_key = settings.SECRET_KEY
+
 
 class SignUpView(APIView):
     def post(self, request):
@@ -53,3 +62,35 @@ class UserDetailView(APIView):
             return Response("권한이 없습니다", status=status.HTTP_403_FORBIDDEN)
         # user.delete()
         # return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# ============================================ 사용자 이메일 인증 ============================================
+
+class UserActivate(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        try:
+            # JWT 토큰 검증
+            payload = jwt.decode(token, secret_key, algorithms=['HS256'])
+            if payload['user_id'] != user.id:
+                return Response('토큰이 올바르지 않습니다', status=status.HTTP_400_BAD_REQUEST)
+        except jwt.ExpiredSignatureError:
+            return Response('만료된 토큰입니다', status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError:
+            return Response('잘못된 토큰입니다', status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print(traceback.format_exc())
+
+        if user is not None:
+            user.is_active = True
+            user.save()
+            return Response(user.email + '계정이 활성화 되었습니다', status=status.HTTP_200_OK)
+        else:
+            return Response('사용자를 찾을 수 없습니다', status=status.HTTP_400_BAD_REQUEST)
