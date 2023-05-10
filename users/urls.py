997415092b8be83aecec330dfda6ecb838b06ff2
chanjulee <crdlckswn@naver.com>
2023-05-10 16:53:50 +0900

    팔로우, 언팔로우 기능 추가

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('sign-in/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('<int:user_id>/', views.UserDetailView.as_view(), name='user_view'),
     path('sign-in/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('follow/<int:user_id>/', views.FollowView.as_view(), name='follow_view'),
 ]
