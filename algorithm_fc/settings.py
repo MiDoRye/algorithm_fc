cb888bc9ba74d45749723b5433ae828f2c602dd5
I Joo Han <juhanlee2218@gmail.com>
2023-05-12 18:53:50 +0900

    테스트 - 1.이메일 인증을 활용한 회원가입 테스트

--- algorithm_fc/settings.py
+++ algorithm_fc/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'users',
@@ -48,6 +49,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -168,6 +170,8 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = '/static/'
--- users/urls.py
+++ users/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('sign-in/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('<int:user_id>/', views.UserDetailView.as_view(), name='user_view'),
     path('sign-in/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('activate/<uidb64>/<token>/',
-         views.UserActivate.as_view(), name='activate'),
+    path('activate/<uidb64>/<token>/', views.UserActivate.as_view(), name='activate'),
     path('follow/<int:user_id>/', views.FollowView.as_view(), name='follow_view'),
 ]
